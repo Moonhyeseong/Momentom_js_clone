98f6824040315cdca2f0f0b1c1e541b931c85f1a
Moonhyeseong <moonhs97@gmail.com>
2021-07-30 02:22:47 +0900

    7.30

--- JS/background.js
+++ JS/background.js
@@ -4,6 +4,6 @@ const images = ['1.jpg', '2.jpg', '3.jpg'];
 const chosenImages = images[Math.floor(Math.random() * images.length)];
 
 const bgimage = document.createElement('img'); //img html 요소 생성
-body.style.backgroundImage = 'url(./img/' + chosenImages + ')';
+body.style.backgroundImage = 'url(img/' + chosenImages + ')';
 // bgimage.src = 'img/' + chosenImages;
 // document.body.appendChild(bgimage);
